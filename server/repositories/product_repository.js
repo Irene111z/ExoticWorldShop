e1f31e7b1542974fd3d522f390dc5d3d44f6c0fa
Iryna Zmiievska <zmievskay2002@gmail.com>
2025-01-06 14:58:16 +0200

    add product service and repository

---
+++ server/repositories/product_repository.js
@@ -0,0 +1,53 @@
+const {Product, ProductFeatures, Category} = require('../models/models')
+
+class ProductRepository{
+    async createProduct(data){
+        return await Product.create(data)
+    }
+
+    async deleteProduct(id){
+        const product = await Product.findByPk(id)
+        if(product){
+            product.destroy()
+        }
+    }
+    async getProduct(id) {
+        return await Product.findOne({
+            where: { id },
+            include: [{ model: ProductFeatures }],
+        });
+    }
+    async getProductById(id) {
+        return await Product.findByPk(id);
+    }
+    async getAllProducts(filter, limit, offset) {
+        return await Product.findAndCountAll({
+            where: filter,
+            limit,
+            offset,
+        });
+    }
+
+    async getSubcategories(parentId) {
+        const subcategories = await Category.findAll({ where: { parentId } });
+        let ids = subcategories.map(sub => sub.id);
+
+        for (const subcategory of subcategories) {
+            const childIds = await this.getSubcategories(subcategory.id);
+            ids = ids.concat(childIds);
+        }
+
+        return ids;
+    }
+    async updateProduct(id, data) {
+        const product = await this.getProductById(id);
+        if (!product) {
+            throw new Error("Товар не знайдено");
+        }
+
+        Object.assign(product, data);
+        return await product.save();
+    }
+}
+
+module.exports = new ProductRepository()
---
+++ server/services/product_service.js
@@ -0,0 +1,59 @@
+const product_repository = require('../repositories/product_repository')
+const uuid = require('uuid')
+const path = require('path')
+const { Op } = require('sequelize');
+
+class ProductService {
+    async createProduct(data, files){
+        const { name, price, disc_price, description, quantity, categoryId, brandId, productFeatures } = data;
+        const { img } = files;
+        const fileName = uuid.v4()+".jpg"
+        img.mv(path.resolve(__dirname, '..','static',fileName))
+
+        const product = await product_repository.createProduct({
+            name, price, disc_price, description, quantity, categoryId, brandId, img: fileName
+        });
+        if (productFeatures) {
+            const features = JSON.parse(productFeatures);
+            for (const feature of features) {
+                await product_repository.addProductFeature({
+                    name: feature.name,
+                    description: feature.description,
+                    productId: product.id,
+                });
+            }
+        }
+        return product;
+    }
+
+    async deleteProduct(id){
+        const product = await product_repository.getProductById(id);
+        if (!product) {
+            throw new Error("Товару не існує");
+        }
+        await product_repository.deleteProduct(id)
+    }
+
+    async getAllProducts(query) {
+        const { brandId, categoryId, limit = 12, page = 1 } = query;
+        const offset = page * limit - limit;
+    
+        let filter = {};
+        if (brandId) filter.brandId = brandId;
+    
+        if (categoryId) {
+            const subcategoryIds = await product_repository.getSubcategories(categoryId);
+            filter.categoryId = { [Op.in]: [categoryId, ...subcategoryIds] };
+        }
+    
+        return await product_repository.getAllProducts(filter, limit, offset);
+    }
+    async getProduct(id) {
+        return await product_repository.getProductById(id);
+    }
+    async changeProduct(id, data) {
+        return await product_repository.updateProduct(id, data);
+    }
+}
+
+module.exports = new ProductService();
